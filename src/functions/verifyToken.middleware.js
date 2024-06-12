43e100e4175c4c3b391572b866516ae8e645a636
yomidaniel <adefuyeabayomi16@gmail.com>
2024-06-12 13:50:51 +0100

    update verify token

--- src/functions/verifyToken.middleware.js
+++ src/functions/verifyToken.middleware.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken")
 // Middleware to check token-based authentication
 function verifyToken(req, res, next) {
-  const token = req.headers["authorization"]
+  const token = req.headers["authorization"].split(' ')[1]
   if (token) {
     jwt.verify(token, process.env.SECRET, (err, decoded) => {
       if (err) {
